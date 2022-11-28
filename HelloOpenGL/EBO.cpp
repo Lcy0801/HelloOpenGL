1260542255a24238db5ec1632a1772ee42d8d435
Chaoying <1754129@gmail.com>
2022-11-28 15:19:44 +0800

    元素缓冲对象绘制多边形

--- HelloOpenGL/EBO.cpp
+++ HelloOpenGL/EBO.cpp
@@ -11,14 +11,14 @@ void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
-}		
+}
 
 int main() {
-	glfwInit();   
+	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
 	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-	GLFWwindow * window = glfwCreateWindow(600, 600, "Hello OpenGL", NULL, NULL);
+	GLFWwindow* window = glfwCreateWindow(600, 600, "Hello OpenGL", NULL, NULL);
 	if (window == NULL) {
 		cout << "创建窗口失败！" << endl;
 		glfwTerminate();
@@ -30,7 +30,7 @@ int main() {
 		cout << "GLAD初始化失败!" << endl;
 		return 0;
 	}
-	const char* vertexShaderSource = 
+	const char* vertexShaderSource =
 		"#version 460 core\n"
 		"layout (location = 0) in vec3 aPos;\n"
 		"void main()\n"
@@ -38,14 +38,14 @@ int main() {
 		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
 		"}\0";
 	unsigned int vertexShader;
-	vertexShader= glCreateShader(GL_VERTEX_SHADER);
+	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
 	int success;
 	char infoLog[512];
 	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
 	if (!success) {
-		cout << "顶点着色器编译失败！"<<endl;
+		cout << "顶点着色器编译失败！" << endl;
 		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
 		cout << infoLog << endl;
 		return 0;
@@ -81,9 +81,17 @@ int main() {
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 	float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f
+	0.5f, 0.5f, 0.0f,   // 右上角
+	0.5f, -0.5f, 0.0f,  // 右下角
+	-0.5f, -0.5f, 0.0f, // 左下角
+	-0.5f, 0.5f, 0.0f   // 左上角
+	};
+	unsigned int indices[] = {
+		// 注意索引从0开始! 
+		// 此例的索引(0,1,2,3)就是顶点数组vertices的下标，
+		// 这样可以由下标代表顶点组合成矩形
+		0, 1, 3, // 第一个三角形
+		1, 2, 3  // 第二个三角形
 	};
 	//初始化顶点数组合对象
 	unsigned int VAO;
@@ -94,21 +102,30 @@ int main() {
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
-	glVertexAttribPointer(0, 3, GL_FLOAT,GL_FALSE, 3*sizeof(float), (void*)0);
+	unsigned int EBO;
+	glGenBuffers(1, &EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 	glBindVertexArray(0);
 	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
 	while (!glfwWindowShouldClose(window)) {
-		processInput(window);	
+		processInput(window);
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
-	//激活着色器
+		//激活着色器
 		glUseProgram(shaderProgram);
 		glBindVertexArray(VAO);
-		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//以线框模式进行绘制
+		glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//以面模式进行绘制
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-	}	
+	}
 	//释放相关资源，退出程序
 	glDeleteVertexArrays(1, &VAO);
 	glDeleteBuffers(1, &VBO);
