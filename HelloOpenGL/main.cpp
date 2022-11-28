6cca9534aa2cc49a6828c13ce634c44ed2d35d3a
Chaoying <1754129@gmail.com>
2022-11-28 14:49:35 +0800

    绘制第一个三角形

--- HelloOpenGL/main.cpp
+++ HelloOpenGL/main.cpp
@@ -11,7 +11,7 @@ void processInput(GLFWwindow* window) {
 	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
 		glfwSetWindowShouldClose(window, true);
 	}
-}
+}		
 
 int main() {
 	glfwInit();   
@@ -27,16 +27,92 @@ int main() {
 	glfwMakeContextCurrent(window);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
-		cout << "GLAD初始化失败!"<<endl;
+		cout << "GLAD初始化失败!" << endl;
 		return 0;
 	}
+	const char* vertexShaderSource = 
+		"#version 460 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\0";
+	unsigned int vertexShader;
+	vertexShader= glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
+	glCompileShader(vertexShader);
+	int success;
+	char infoLog[512];
+	glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		cout << "顶点着色器编译失败！"<<endl;
+		glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+		cout << infoLog << endl;
+		return 0;
+	}
+	const char* fragmentShaderSource =
+		"#version 460 core\n"
+		"out vec4 FragColor;\n"
+		"void main(){\n"
+		"FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);}\0";
+	unsigned int fragmentShader;
+	fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
+	glCompileShader(fragmentShader);
+	glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
+	if (!success) {
+		cout << "片段着色器编译失败!" << endl;
+		glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
+		cout << infoLog << endl;
+		return 0;
+	}
+	unsigned shaderProgram;
+	shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vertexShader);
+	glAttachShader(shaderProgram, fragmentShader);
+	glLinkProgram(shaderProgram);
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
+	if (!success) {
+		cout << "着色器链接失败!" << endl;
+		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+		cout << infoLog << endl;
+		return 0;
+	}
+	glDeleteShader(vertexShader);
+	glDeleteShader(fragmentShader);
+	float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f
+	};
+	//初始化顶点数组合对象
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	//初始化顶点缓冲合对象
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	glVertexAttribPointer(0, 3, GL_FLOAT,GL_FALSE, 3*sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
 	while (!glfwWindowShouldClose(window)) {
-		processInput(window);
+		processInput(window);	
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
+	//激活着色器
+		glUseProgram(shaderProgram);
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 3);
 		glfwSwapBuffers(window);
 		glfwPollEvents();
-	}
+	}	
+	//释放相关资源，退出程序
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram);
 	glfwTerminate();
 	return 0;
 }
