8bebedf083bec0d7dbe809945b08297d5c7cdfb0
Chaoying <1754129@gmail.com>
2022-12-10 16:54:46 +0800

    相机系统

---
+++ HelloOpenGL/摄像机.cpp
@@ -0,0 +1,280 @@
+#include <glad/glad.h>
+#include <glfw3.h>
+#include <iostream>
+#include "stb_image.h"
+#include <glm.hpp>
+#include <gtc/matrix_transform.hpp>
+#include <gtc/type_ptr.hpp>
+#include "Shader.h"
+using namespace std;
+
+#define VIEWWIDTH 1000;
+#define VIEWHEIGHT 1000;
+
+//相机位置
+glm::vec3 cameraPos(0, 0, 3);
+//相机朝向
+glm::vec3 cameraFront(0, 0, -0.1);
+//相机天顶方向
+glm::vec3 cameraUp(0, 1, 0);
+//相机移动速度
+float cameraSpeed = 5.0;
+//渲染时间
+float currentTime, lastTime;
+float timeDelta = 0;
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height) {
+	glViewport(0, 0, width, height);
+}
+
+
+void processInput(GLFWwindow* window) {
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+		glfwSetWindowShouldClose(window, true);
+	}
+	//向前移动
+	if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS) {
+		cameraPos += cameraFront*cameraSpeed*timeDelta;
+	}
+	//向后移动
+	if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS) {
+		cameraPos -= cameraFront*cameraSpeed*timeDelta;
+	}
+	//向左移动
+	if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS) {
+		cameraPos -= (glm::cross(cameraFront, cameraUp))*cameraSpeed*timeDelta;
+	}
+	//向右移动
+	if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS) {
+		cameraPos += (glm::cross(cameraFront, cameraUp))*cameraSpeed*timeDelta ;
+	}
+}
+
+//
+double lastXpos, lastYpos;
+bool firstFlag = true;
+double sensitive = 0.05;
+double heading = -90;
+double pitch = 0;
+void cursorPos_callback(GLFWwindow * window,double xPos,double yPos) {
+	if (glfwGetMouseButton(window,GLFW_MOUSE_BUTTON_LEFT)==false)
+	{
+		return;
+	}
+	if (firstFlag) {
+		firstFlag = false;
+		lastXpos = xPos;
+		lastYpos = yPos;
+	}
+	double xoffset = xPos - lastXpos;
+	double yoffset = yPos - lastYpos;
+	heading += xoffset * sensitive;
+	pitch += yoffset * sensitive;
+	if (pitch >= 90) {
+		pitch = 89.99999;
+	}
+	if (pitch <= -90) {
+		pitch = -89.99999;
+	}
+	cameraFront = glm::vec3(glm::cos(glm::radians(pitch)) * glm::cos(glm::radians(heading)),
+		glm::cos(glm::radians(pitch) * glm::sin(glm::radians(heading))),
+		glm::sin(glm::radians(pitch))
+	);
+}
+
+int main() {
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	GLFWwindow* window = glfwCreateWindow(1000, 1000, "Hello OpenGL", NULL, NULL);
+	if (window == NULL) {
+		cout << "创建窗口失败！" << endl;
+		glfwTerminate();
+		return 0;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetCursorPosCallback(window, cursorPos_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		cout << "GLAD初始化失败!" << endl;
+		return 0;
+	}
+	// 顶点数据
+	float vertices[] = {
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+			-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+			-0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
+			-0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+
+			 0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+			 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 1.0f,
+			 0.5f, -0.5f,  0.5f,  1.0f, 1.0f,
+			-0.5f, -0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+			-0.5f,  0.5f, -0.5f,  0.0f, 0.0f,
+			 0.5f,  0.5f, -0.5f,  1.0f, 0.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+			-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+			-0.5f,  0.5f, -0.5f,  0.0f, 0.0f
+	};
+
+	//初始化顶点数组合对象
+	unsigned int VAO;
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	//初始化顶点缓冲合对象
+	unsigned int VBO;
+	glGenBuffers(1, &VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+	//初始化着色器
+	Shader shaderProgram("./3d.vert", "./3d.frag");
+	if (shaderProgram.getId() == -1) {
+		cout << "着色器生成失败!" << endl;
+		return 0;
+	}
+	//顶点数据与着色器顶点属性之间的链接关系
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+	//创建第一个纹理对象
+	//初始化纹理对象
+	int width, height, nChannels;
+	unsigned char* textureData1 = stbi_load("./texture.png", &width, &height, &nChannels, 0);
+	if (!textureData1) {
+		cout << "载入纹理图像失败!" << endl;
+		return 0;
+	}
+	unsigned int texture1;
+	glGenTextures(1, &texture1);
+	glBindTexture(GL_TEXTURE_2D, texture1);
+	//设置纹理的缠绕方式
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//设置纹理的过滤（重采样方式）
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//设置纹理图案
+	//注意四通道图像一定要以RGBA的形式读取
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData1);
+	//生成多级渐远纹理
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(textureData1);
+	//创建第二个纹理对象
+	unsigned char* textureData2 = stbi_load("./face.png", &width, &height, &nChannels, 0);
+	if (!textureData2) {
+		cout << "载入纹理图像失败!" << endl;
+		return 0;
+	}
+	unsigned int texture2;
+	glGenTextures(1, &texture2);
+	glBindTexture(GL_TEXTURE_2D, texture2);
+	//设置纹理的缠绕方式
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+	//设置纹理的过滤（重采样方式）
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+	glTextureParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+	//设置纹理图案
+	//注意四通道图像一定要以RGBA的形式读取
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, textureData2);
+	//生成多级渐远纹理
+	glGenerateMipmap(GL_TEXTURE_2D);
+	stbi_image_free(textureData2);
+	//将片段着色器多个采样器与纹理对象一一对应起来
+	shaderProgram.use();
+	shaderProgram.setInt("textureUnit1", 0);
+	shaderProgram.setInt("textureUnit2", 1);
+	//恢复到初始化状态
+	glBindVertexArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);
+	glBindTexture(GL_TEXTURE_2D, 0);
+	glUseProgram(0);
+	//开启深度测试
+	glEnable(GL_DEPTH_TEST);
+	float rAangle = 0;
+	while (!glfwWindowShouldClose(window))
+	{
+		currentTime = glfwGetTime();
+		if (lastTime) {
+			timeDelta = currentTime - lastTime;
+		}
+		else
+		{
+			timeDelta = 0;
+		}
+		lastTime = currentTime;
+		processInput(window);
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+		//激活纹理单元
+		glActiveTexture(GL_TEXTURE0);
+		glBindTexture(GL_TEXTURE_2D, texture1);
+		//激活纹理单元
+		glActiveTexture(GL_TEXTURE1);
+		glBindTexture(GL_TEXTURE_2D, texture2);
+		//激活着色器
+		shaderProgram.use();
+		//定义变换矩阵
+		//当前帧的旋转角
+		rAangle = rAangle;
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::rotate(model, glm::radians(rAangle), glm::vec3(1.0f, 1.0f, 1.0f));
+		glm::mat4 view = glm::mat4(1.0f);
+		//view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		view = glm::lookAt(cameraPos, cameraFront, cameraUp);
+		glm::mat4 proj = glm::mat4(1.0f);
+		proj = glm::perspective(glm::radians(45.0f), 1.0f, 0.1f, 100.0f);
+		//设施着色器中的变化矩阵
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.getId(), "model"), 1, false, glm::value_ptr(model));
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.getId(), "view"), 1, false, glm::value_ptr(view));
+		glUniformMatrix4fv(glGetUniformLocation(shaderProgram.getId(), "proj"), 1, false, glm::value_ptr(proj));
+		//绑定顶点数组对象
+		glBindVertexArray(VAO);
+		//以线框模式进行绘制
+		//glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		//以面模式进行绘制
+		glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+		//glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+	//释放相关资源，退出程序
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glDeleteProgram(shaderProgram.getId());
+	glDeleteTextures(1, &texture1);
+	glDeleteTextures(1, &texture2);
+	glfwTerminate();
+	return 1;
+}
